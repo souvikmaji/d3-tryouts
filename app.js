9c087f56cba8e14c8c1d213486029724bee5e7bf
souvikmaji <souvikmaji94@gmail.com>
2020-04-01 22:56:40 +0530

    replaced jade view engine with hbs

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "html");
+app.engine("html", require("hbs").__express); // use html extension for hbs files
 
 app.use(logger("dev"));
 app.use(express.json());
