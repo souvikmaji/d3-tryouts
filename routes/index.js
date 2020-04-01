36f5097232a497f5e21a7c29cdbf1b53a93507e9
souvikmaji <souvikmaji94@gmail.com>
2020-04-01 22:47:16 +0530

    loading time series data

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require("express");
 var router = express.Router();
+const r2 = require("r2");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-	res.render("index", { title: "Express" });
+router.get("/", async function(req, res, next) {
+	const data = await loadData();
+	res.render("index", { title: "Express", data: data });
 });
+
+async function loadData() {
+	try {
+		const timeSeriesData = await loadTimeSeriesData();
+		return {timeSeriesData: timeSeriesData};
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+async function loadTimeSeriesData() {
+	const url = "https://api.covid19india.org/data.json";
+	return r2(url).json;
+}
+
 
 module.exports = router;
